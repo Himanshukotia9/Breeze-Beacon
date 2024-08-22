2677a78b9e9dcf9324d724d5b811a5cc78792ce0
Himanshu kotia <himanshukotia9@gmail.com>
2024-08-23 00:49:09 +0530

    background updated

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Loader({title}) {
   return (
-    <div className='flex justify-center items-center min-h-screen'>
+    <div className='flex justify-center items-center min-h-screen bg-background bg-cover bg-center bg-no-repeat'>
         <div className="flex items-center justify-center bg-cover bg-center bg-no-repeat">
             <div className="text-center bg-weather bg-opacity-50 p-8 mx-4 rounded-lg max-w-md">
                 <div className="text-6xl mb-6">
